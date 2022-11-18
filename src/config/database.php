d42d2725281c713c40d01dac095ac6c0f3561842
NguyenMinhQuan <minhquanhbt@gmail.com>
2022-11-18 22:19:45 +0700

    Update database.php

--- src/config/database.php
+++ src/config/database.php
@@ -46,8 +46,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             // 'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST'),
-            'port' => env('DB_PORT'),
+            'host' => env('DB_HOST',''),
+            'port' => env('DB_PORT',''),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
